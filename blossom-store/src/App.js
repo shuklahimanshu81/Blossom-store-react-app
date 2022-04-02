1927f71a5b526a63608f7fbfd5a34535822a4d85
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2022-04-02 14:47:11 +0530

    feat : Completed footer components and moved styles to App.css

--- blossom-store/src/App.js
+++ blossom-store/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
-
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
 
 function App() {
+  
   return (
     <div className="App">
-     
+      <Header />
+      <Footer />
     </div>
   );
 }
---
+++ blossom-store/src/Components/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () =>{
+    return(
+        <div>
+            <footer className="blossom-footer">
+            
+               <ul className="quick-link-list">
+                  <li className="quick-link-item"> About Us </li>
+                  <li className="quick-link-item"> Terms & Conditions </li>
+                  <li className="quick-link-item"> Privacy policy </li>
+               </ul>           
+               <ul className="social-media">
+                  <li className="social-media-item"> <a href="/"> Github </a> </li>
+                  <li className="social-media-item"> <a href="/"> Twitter </a> </li>
+                  <li className="social-media-item">  <a href="/"> Instagram</a> </li>
+               </ul>
+            
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
