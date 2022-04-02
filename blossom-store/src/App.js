dc765472ee23cced4546f9a613db313dde0dc35e
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2022-04-02 22:43:28 +0530

    feat : completed landing screen

--- blossom-store/src/App.js
+++ blossom-store/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
+import Footer from "./Components/Home/Footer";
+import Header from "./Components/Home/Header";
+import Landing from "./Components/Home/Landing";
 
 function App() {
   
   return (
     <div className="App">
       <Header />
+      <Landing />
       <Footer />
     </div>
   );
